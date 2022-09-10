812fea55e066bc3d3d220aaab10b24a029167471
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-10 21:02:08 +0530

    series of numbers

---
+++ ObjectOriented.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class animal {
+    public:
+        string sound;
+        int count;
+    private:
+        cout >> "Hi, I'm an Animal!";
+};
+
+int main(){
+    animal cow;
+    cow.sound = "moo";
+    cow.count = 12;
+    return 0;
+}
---
+++ Series.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    short int i = 10;
+
+    while( i != -2){
+        i++;
+        cout << i << endl;
+    }
+
+    return 0;
+}
